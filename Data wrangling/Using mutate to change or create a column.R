08ffb5b98fad9acae67cfd1401bbe0a0e20263d2
Alex Allenspach <61338786+alexallenspach@users.noreply.github.com>
2020-10-12 17:30:41 -0600

    Create Using mutate to change or create a column.R

---
+++ Data wrangling/Using mutate to change or create a column.R
@@ -0,0 +1,17 @@
+# Suppose we want life expectancy to be measured in months instead of years: you'd have to multiply the existing value by 12. You can use the mutate() verb to change 
+# this column, or to create a new column that's calculated this way.
+
+
+#    Use mutate() to change the existing lifeExp column, by multiplying it by 12: 12 * lifeExp.
+#    Use mutate() to add a new column, called lifeExpMonths, calculated as 12 * lifeExp.
+
+library(gapminder)
+library(dplyr)
+
+# Use mutate to change lifeExp to be in months
+gapminder %>%
+  mutate(lifeExp = lifeExp * 12)
+
+# Use mutate to create a new column called lifeExpMonths
+gapminder %>%
+  mutate(lifeExpMonths = lifeExp * 12)
