900e06d9be403875217283c4adc8bf5fd5adc16c
Alex Allenspach <61338786+alexallenspach@users.noreply.github.com>
2020-10-12 19:53:53 -0600

    Create Summarizing by continent and year.R

---
+++ Grouping and summarizing/Summarizing by continent and year.R
@@ -0,0 +1,13 @@
+# Instead of grouping just by year, or just by continent, you'll now group by both continent and year to summarize within each.
+
+
+#    Find the median life expectancy (lifeExp) and maximum GDP per capita (gdpPercap) within each combination of continent and year, saving them into medianLifeExp and
+#    maxGdpPercap, respectively.
+
+library(gapminder)
+library(dplyr)
+
+# Find median life expectancy and maximum GDP per capita in each continent/year combination
+gapminder %>%
+  group_by(continent, year) %>%
+  summarize(medianLifeExp = median(lifeExp), maxGdpPercap = max(gdpPercap))
