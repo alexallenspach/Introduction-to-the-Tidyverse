ce08c488e802d110f712945dd2282e921a197230
Alex Allenspach <61338786+alexallenspach@users.noreply.github.com>
2020-10-12 18:52:50 -0600

    Create Adding size and color to a plot.R

---
+++ Data visualization/Adding size and color to a plot.R
@@ -0,0 +1,17 @@
+# In the last exercise, you created a scatter plot communicating information about each country's population, life expectancy, and continent. Now you'll use the size of
+# the points to communicate even more.
+
+
+#    Modify the scatter plot so that the size of the points represents each country's GDP per capita (gdpPercap).
+
+library(gapminder)
+library(dplyr)
+library(ggplot2)
+
+gapminder_1952 <- gapminder %>%
+  filter(year == 1952)
+
+# Add the size aesthetic to represent a country's gdpPercap
+ggplot(gapminder_1952, aes(x = pop, y = lifeExp, color = continent, size = gdpPercap)) +
+  geom_point() +
+  scale_x_log10()
