e7d0e25c49b3f6eb7b6c4a2b1a00a5ed38730115
Alex Allenspach <61338786+alexallenspach@users.noreply.github.com>
2020-10-14 10:38:36 -0600

    Create Visualizing population with x-axis on a log scale.R

---
+++ Types of visualizations/Visualizing population with x-axis on a log scale.R
@@ -0,0 +1,21 @@
+# In the last exercise you created a histogram of populations across countries. You might have noticed that there were several countries with a much higher population 
+# than others, which causes the distribution to be very skewed, with most of the distribution crammed into a small part of the graph. (Consider that it's hard to tell 
+# the median or the minimum population from that histogram).
+
+# To make the histogram more informative, you can try putting the x-axis on a log scale.
+
+
+#    Use the gapminder_1952 dataset (code is provided) to create a histogram of country population (pop) in the year 1952, putting the x-axis on a log scale with 
+#    scale_x_log10().
+
+library(gapminder)
+library(dplyr)
+library(ggplot2)
+
+gapminder_1952 <- gapminder %>%
+  filter(year == 1952)
+
+# Create a histogram of population (pop), with x on a log scale
+ggplot(gapminder_1952, aes(x = pop)) +
+  geom_histogram(bins = 50) +
+  scale_x_log10()
